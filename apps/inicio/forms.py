ccd8ac8ad9f84a23c560886ea5c2e4b6f2981dae
Kevin Vidal <kefevimi@gmai.com>
2021-10-14 15:09:20 -0500

    un avance

--- apps/inicio/forms.py
+++ apps/inicio/forms.py
@@ -5,8 +5,18 @@ class login_form(forms.Form):
     email = forms.EmailField(label="Correo", widget=forms.TextInput)
     password = forms.CharField(label="Contraseña", widget=forms.PasswordInput(render_value=False))
 
-class usuario_form(forms.ModelForm):
-    model = Usuario
+class register_form(forms.Form):
+    email = forms.EmailField(label="Correo", widget=forms.TextInput)
+    password1 = forms.CharField(label="Contraseña", widget=forms.PasswordInput(render_value=False))
+    password2 = forms.CharField(label="Confirmar Contraseña", widget=forms.PasswordInput(render_value=False))
+
+class medico_form(forms.ModelForm):
+    model = Medico
+    field = '__all__'
+    exclude = ['']
+
+class paciente_form(forms.ModelForm):
+    model = Paciente
     field = '__all__'
     exclude = ['']
 
--- apps/inicio/views.py
+++ apps/inicio/views.py
@@ -29,13 +29,43 @@ def login_view(request):
     return render(request,'inicio/login.html', locals())
 
 def registro_view(request):
+    if request.method == 'POST':
+        form_u = register_form(request.POST)
+        form_p = paciente_form(request.POST)
+        if form_u.is_valid() and form_p.is_valid():
+            ema = form_u.cleaned_data['email']
+            pas = form_u.cleaned_data['password']
+
+            u = User.objects.create_user(username=ema, password=pas)
+            p=form_p.save(commit=False)
+            u.save()
+            p.user=u
+            p.save()
+
+            return redirect('/inicio/')
+
     return render(request,'inicio/registro.html', locals())
 
 def registro_medico_view(request):
+    if request.method == 'POST':
+        form_u = register_form(request.POST)
+        form_m = medico_form(request.POST)
+        if form_u.is_valid() and form_m.is_valid():
+            ema = form_u.cleaned_data['email']
+            pas = form_u.cleaned_data['password']
+
+            u = User.objects.create_user(username=ema, password=pas)
+            p=form_m.save(commit=False)
+            u.save()
+            p.user=u
+            p.save()
+
+            return redirect('/inicio/')
     return render(request,'inicio/registro.html', locals())
 
 def logout_view(request):
-    return render(request,'inicio/logout.html', locals())
+    logout(request)
+    return redirect('inicio')
 
 def reportar_sintomas_view(request):
     return render(request,'inicio/reportar_sintomas.html', locals())
@@ -46,7 +76,8 @@ def cambiar_medico_view(request):
 def historial_view(request):
     return render(request,'inicio/historial.html', locals())
 
-def paciente_detalle_view(request):
+def paciente_detalle_view(request, id_paciente):
+    object = Paciente.object.get(id = id_paciente)
     return render(request,'inicio/paciente_detalle.html', locals())
 
 def generar_orden_view(request):
