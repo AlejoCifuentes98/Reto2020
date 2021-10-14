4ef708029e0ef0749850dcece697e5e4ac680bb6
Kevin Vidal <kefevimi@gmai.com>
2021-10-14 08:44:23 -0500

    algo de el modelo

--- apps/inicio/models.py
+++ apps/inicio/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth import User
 
 # Create your models here.
+
+class Usuario(models.Model):
+    nombre      = models.CharField(max_length=250)
+    apellido    = models.CharField(max_length=250)
+    telefono    = models.IntegerField()
+    fecha_nacimineto = models.DateField()
+    user       = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+    
+class Mensaje(models.Model):
+    mensaje = models.TextField()
+    hora    = models.DateField(auto_now_add=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.mensaje
+
+class GrupoFamiliar(models.Model):
+    nombre = models.CharField(max_length=250)
+    medico = models.ForeignKey(Medico, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+class OrdenMedica(models.Model):
+    orden  = models.TextField()
+    fecha  = models.DateTimeField(auto_now_add=True)
+
+class Especialista(models.Model):
+    
+
+class Remisiones(models.Model):
+
+
+    
+
+
