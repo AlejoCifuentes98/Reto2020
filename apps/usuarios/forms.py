3c9cf11bce0f64699b6229b88d2c0f6d75da0b30
Kevin Vidal <kefevimi@gmai.com>
2021-10-15 08:38:34 -0500

    metodos clean en el form

--- apps/usuarios/forms.py
+++ apps/usuarios/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Medico, Paciente
+from django.contrib.auth.models import User
 
 class login_form(forms.Form):
     email = forms.EmailField(label="Correo", widget=forms.TextInput)
@@ -9,6 +10,24 @@ class register_form(forms.Form):
     email = forms.EmailField(label="Correo", widget=forms.TextInput)
     password1 = forms.CharField(label="Contraseña", widget=forms.PasswordInput(render_value=False))
     password2 = forms.CharField(label="Confirmar Contraseña", widget=forms.PasswordInput(render_value=False))
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+       
+        try:
+            c = User.objects.get(username = email)
+        except User.DoesNotExist:
+            return email
+        raise forms.ValidationError('El correo ingresado, ya se encuentra registrado')
+
+    def clean_password2(self):
+        password1 = self.cleaned_data['password1']
+        password2 = self.cleaned_data['password2']
+        if password1 == password2:
+            return password2
+        else:
+            raise forms.ValidationError('Las contraseñas no coinciden, intente de nuevo')
+
 
 class register_grupo_form(forms.ModelForm):
     model = Paciente
