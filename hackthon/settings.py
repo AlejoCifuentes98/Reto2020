ed64cbd5396cd50f1103ebea4af5944819c0263a
KevinFernandoVidal <kefevimi@gmail.com>
2021-10-18 00:30:15 -0500

    listo

--- hackthon/settings.py
+++ hackthon/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.inicio',
     'apps.usuarios',
+    'apps.pacientes',
+    'apps.medico',
 ]
 
 MIDDLEWARE = [
--- hackthon/urls.py
+++ hackthon/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.inicio.urls')),
     path('', include('apps.usuarios.urls')),
+    path('', include('apps.medico.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
