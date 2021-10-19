15671fa23fda5612eba421f2df895ce704ced796
Mario Cifuentes <macifuentes941@misena.edu.co>
2021-10-19 09:11:31 -0500

    Configuracion BD

--- hackthon/settings.py
+++ hackthon/settings.py
@@ -72,17 +72,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'hackthon.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.emisalud',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
